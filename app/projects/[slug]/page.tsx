9ed9b980fb7cd62b3a559edb56543606109ad7d2
yshv-2022 <yashasvichaudhary14@gmail.com>
2023-08-13 16:56:10 +0530

    Add about page

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,11 +1,9 @@
 import { notFound } from "next/navigation";
-// import { allProjects } from "contentlayer/generated";
 import { Mdx } from "@/app/components/mdx";
 import { Header } from "./header";
 import "./mdx.css";
 import { ReportView } from "./view";
 import { allProjects } from "@/constants/projects";
-// import { Redis } from "@upstash/redis";
 
 export const revalidate = 60;
 
@@ -14,8 +12,6 @@ type Props = {
     slug: string;
   };
 };
-
-// const redis = Redis.fromEnv();
 
 export async function generateStaticParams(): Promise<Props["params"][]> {
   return allProjects
@@ -34,17 +30,11 @@ export default async function PostPage({ params }: Props) {
   }
 
   const views = 0;
-  //   const views =
-  //     (await redis.get<number>(["pageviews", "projects", slug].join(":"))) ?? 0;
 
   return (
     <div className="bg-zinc-50 min-h-screen">
       <Header project={project} views={views} />
       <ReportView slug={project.slug} />
-      {/* 
-      <article className="px-4 py-12 mx-auto prose prose-zinc prose-quoteless">
-        <Mdx code={project.body.code} />
-      </article> */}
     </div>
   );
 }
